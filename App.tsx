06e65829afd94d50dfe6d74d125c375d12f31fe4
Harys vizcaino <harys@mctekk.com>
2020-04-02 22:21:06 -0400

    until here the device can receive a call and make a call only to the server

--- App.tsx
+++ App.tsx
@@ -8,7 +8,9 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect } from 'react';
+import TwilioVoice from 'react-native-twilio-programmable-voice'
+
 import {
   SafeAreaView,
   StyleSheet,
@@ -16,6 +18,8 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
+  Platform,
 } from 'react-native';
 
 import {
@@ -26,9 +30,86 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
+const apiUrl = 'https://f8cb8689.eu.ngrok.io/accessToken';
+
 declare var global: {HermesInternal: null | {}};
 
 const App = () => {
+
+  const getAccessTokenFromServer = async () => {
+    const request = await fetch(apiUrl);
+    const resp = await request.json();
+    console.log(resp);
+    return resp.token;
+  }
+
+  const initTelephony = async () => {
+    try {
+        const accessToken = await getAccessTokenFromServer()
+        console.log('token', accessToken);
+        const success = await TwilioVoice.initWithToken(accessToken, )
+        console.log('success', success);
+        if (Platform.OS === 'ios') { //required for ios
+          TwilioVoice.configureCallKit({
+            appName: 'mcvoice',
+          });
+        }
+    } catch (err) {
+        console.error(err)
+    }
+
+}
+
+const CallSomeone = () => {
+  console.log('Calling!!!');
+  // set speaker
+  TwilioVoice.setSpeakerPhone(true);
+  // start a call
+TwilioVoice.connect({To: 'Alice'})
+}
+
+const CallAccept = () => {
+  console.log('INCOMMING');
+  TwilioVoice.accept();
+}
+
+
+ // iOS Only
+ const initTelephonyWithUrl = (url) => {
+  TwilioVoice.initWithTokenUrl(url)
+  try {
+      TwilioVoice.configureCallKit({
+          appName:       'TwilioVoiceExample',                  // Required param
+          imageName:     'my_image_name_in_bundle',             // OPTIONAL
+          ringtoneSound: 'my_ringtone_sound_filename_in_bundle' // OPTIONAL
+      })
+  } catch (err) {
+      console.err(err)
+  }
+}
+
+
+// a few list of events 
+const Listeners = () => {
+  console.log('Listening!!');
+  // add listeners (flowtype notation)
+TwilioVoice.addEventListener('deviceReady', function() {
+  // no data
+  console.warn('WE READYYY!!!')
+})
+TwilioVoice.addEventListener('deviceNotReady', function(data) {
+  console.error('SOMETHING BAD', data);
+})
+}
+
+useEffect(() => {
+  console.log({ TwilioVoice });
+  console.log('Running');
+  initTelephony();
+  Listeners();
+}, [])
+
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
@@ -43,6 +124,40 @@ const App = () => {
           )}
           <View>
             <Text>HI testing</Text>
+            <TouchableOpacity 
+            style={{
+              backgroundColor: 'red',
+              marginTop: 20,
+              padding: 20,
+              alignItems: 'center'
+            }}
+            onPress={CallSomeone}
+            >
+              <Text
+              style={{
+                color: '#FFFFFF',
+                fontSize: 26
+              }}
+              >Call Someone</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity 
+            style={{
+              backgroundColor: 'green',
+              marginTop: 20,
+              padding: 20,
+              alignItems: 'center'
+            }}
+            onPress={CallSomeone}
+            >
+              <Text
+              style={{
+                color: '#FFFFFF',
+                fontSize: 26
+              }}
+              >Incoming Call!!</Text>
+            </TouchableOpacity>
+
           </View>
         </ScrollView>
       </SafeAreaView>
